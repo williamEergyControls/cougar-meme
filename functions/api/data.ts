116751b657c0a62bab52108d9b20ff4b3f6c2ba3
williamEergyControls <william.hardingenergycontrols@gmail.com>
2026-04-29 18:44:27 -0500

    data.ts

--- functions/api/data.ts
+++ functions/api/data.ts
@@ -1,1 +1,16 @@
 
+export const onRequest = async () => {
+  const cities = ["Dallas","Houston","Austin","Fort Worth"];
+
+  const feed = Array.from({length:5}).map(() => ({
+    city: cities[Math.floor(Math.random()*cities.length)],
+    minutes: Math.floor(Math.random()*10)+1
+  }));
+
+  return new Response(JSON.stringify({
+    count: Math.floor(Math.random()*5)+3,
+    feed
+  }), {
+    headers: { "content-type": "application/json" }
+  });
+};
