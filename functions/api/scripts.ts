40f50fe2f0de2297aeb0fed5edf26a67f7758025
williamEergyControls <william.hardingenergycontrols@gmail.com>
2026-04-29 18:44:55 -0500

    scripts.ts

--- functions/api/scripts.ts
+++ functions/api/scripts.ts
@@ -1,1 +1,25 @@
+export const onRequest = async () => {
+  const hooks = [
+    "I checked cougars near me in Texas",
+    "DO NOT search cougars near you",
+    "This was a mistake",
+    "I regret checking this"
+  ];
 
+  const trends = [
+    "yoga session detected",
+    "coffee break spotted",
+    "night hunt mode",
+    "gym activity spike"
+  ];
+
+  const scripts = Array.from({length:100}).map(() => {
+    return {
+      text: `${hooks[Math.floor(Math.random()*hooks.length)]}... ${trends[Math.floor(Math.random()*trends.length)]} 💀`
+    };
+  });
+
+  return new Response(JSON.stringify(scripts), {
+    headers: { "content-type": "application/json" }
+  });
+};
